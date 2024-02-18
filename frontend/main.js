0ded1f1d9b42fdd954ce3527e085e68ebd3b0115
Michael Thome <techisgreat@duck.com>
2024-02-18 15:11:29 -0600

    updates to readme

--- frontend/main.js
+++ frontend/main.js
@@ -1,19 +1,20 @@
-window.addEventListener('DOMContentLoaded'), (event) =>{
+window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
-})
+});
 
-const functionApi = '' ;
+const functionApi = '';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
-        return response.json()
-    }).then(response =>{
-        console.log("Website called function API.");
-        count = response.count;
-        document.getElementById("counter").innerText = count;
-    }).catch(function(error){
-        console.log(error);
-    });
+    fetch(functionApi)
+        .then(response => response.json())
+        .then(response => {
+            console.log("Website called function API.");
+            count = response.count;
+            document.getElementById("counter").innerText = count;
+        })
+        .catch(error => {
+            console.log(error);
+        });
     return count;
-}
+};
