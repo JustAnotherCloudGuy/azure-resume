774ee778fa27b32a46da6d1313d3266ea2c89f09
Michael Thome <techisgreat@duck.com>
2024-03-06 18:25:26 -0600

    updated main.js

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 });
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://azureresumelab.azurewebsites.net/api/GetResumeCounter?code=KQdo3POR2biDSSl96jvWmv2pv_zOc1GhtuC_SzYyyaHQAzFu3GsVRg==';
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi)
+    fetch(functionApiUrl)
         .then(response => response.json())
         .then(response => {
             console.log("Website called function API.");
